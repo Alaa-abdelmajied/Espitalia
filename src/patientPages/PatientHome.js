aadb704e831f83c3937c9eff00f0b6cbb693b722
Maram Ghazal <Maram Ghazal>
2022-05-21 04:06:58 +0200

    handling typo in patient home

--- src/patientPages/PatientHome.js
+++ src/patientPages/PatientHome.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   StyleSheet,
   Text,
@@ -10,17 +10,17 @@ import {
   Pressable,
   FlatList,
   ActivityIndicator,
-  RefreshControl
+  RefreshControl,
 } from 'react-native';
 
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import { Rating } from 'react-native-ratings';
+import {Rating} from 'react-native-ratings';
 
-import { Server_URL } from '@env';
-import { useIsFocused } from '@react-navigation/native';
+import {Server_URL} from '@env';
+import {useIsFocused} from '@react-navigation/native';
 
-export default function PatientHome({ navigation }) {
+export default function PatientHome({navigation}) {
   const [hospitals, setHospitals] = useState([]);
   const [doctors, setDoctors] = useState([]);
   const [specializations, setSecializations] = useState([]);
@@ -43,7 +43,6 @@ export default function PatientHome({ navigation }) {
         setLoadData(false);
         setRefreshing(false);
       });
-
   };
 
   useEffect(() => {
@@ -61,7 +60,7 @@ export default function PatientHome({ navigation }) {
 
   const onRefreshing = () => {
     setRefreshing(true);
-  }
+  };
 
   const onPressHospital = (id, name, address) => {
     navigation.navigate('SpecializationScreen', {
@@ -113,184 +112,179 @@ export default function PatientHome({ navigation }) {
     navigation.navigate('HospitalList');
   };
   const seeAllDoctors = () => {
-    navigation.navigate('DoctorList', { isAllDoctors: true });
+    navigation.navigate('DoctorsScreen', {isAllDoctors: true});
   };
   const seeAllSpecializations = () => {
-    navigation.navigate('SpecializationScreen',
-      {
-        isAllSpecializations: true,
-      }
-    );
+    navigation.navigate('SpecializationScreen', {
+      isAllSpecializations: true,
+    });
   };
 
-  return (
-    loadData ?
-      <View style={styles.loadingIcon}>
-        <ActivityIndicator size="large" color="#0451cc" />
-      </View> :
-      <ScrollView vertical={true}
-        refreshControl={
-          <RefreshControl
-            refreshing={refreshing}
-            onRefresh={onRefreshing}
-          />
-        }
-      >
-        <View style={styles.header}>
-          <Image
-            style={styles.Image}
-            source={require('../../images/app_logo-removebg-preview.png')}></Image>
-          <Text style={styles.headerText}>espitalia</Text>
-        </View>
-        <View style={styles.headline}>
-          <Text style={styles.titleText}>Hospitals</Text>
-          <Pressable>
-            <Text
-              style={{ color: '#1c1bad', textDecorationLine: 'underline' }}
-              onPress={seeAllHospitals}>
-              All hospitals
-            </Text>
-          </Pressable>
-        </View>
-        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-          {hospitals.map((card, cardIndex) => {
-            return (
-              <TouchableOpacity
-                style={styles.cards}
-                key={cardIndex}
-                onPress={() =>
-                  onPressHospital(card._id, card.name, card.address)
-                }>
-                <View style={styles.card_header}>
-                  <Text style={styles.name}> {card.name} </Text>
-                </View>
-                <View style={styles.hospital_content}>
-                  <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                    <Ionicons
-                      name={'location-sharp'}
-                      size={25}
-                      color="#1c1bad"
-                      style={{ margin: 10 }}></Ionicons>
-                    <Text style={styles.cardText}>{card.address}</Text>
-                  </View>
-                </View>
-              </TouchableOpacity>
-            );
-          })}
-        </ScrollView>
-        {/* <View style={styles.lineStyle} /> */}
-        <View style={styles.headline}>
-          <Text style={styles.titleText}>Doctors</Text>
-          <Pressable>
-            <Text
-              style={{ color: '#1c1bad', textDecorationLine: 'underline' }}
-              onPress={seeAllDoctors}>
-              All doctors
-            </Text>
-          </Pressable>
-        </View>
-        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-          {doctors.map((card, cardIndex) => {
-            return (
-              <TouchableOpacity
-                style={styles.cards}
-                key={cardIndex}
-                onPress={() =>
-                  onPressDoctor(
-                    card._id,
-                    card.name,
-                    card.speciality,
-                    card.hospitalName,
-                    card.hospitalAddress,
-                    card.averageRating,
-                  )
-                }>
-                <View style={styles.card_header}>
-                  <Image
-                    style={styles.doctorImg}
-                    source={{
-                      uri: 'https://bootdey.com/img/Content/avatar/avatar6.png',
-                    }}></Image>
-                  <View style={styles.textView}>
-                    <Text style={styles.name}>Dr. {card.name} </Text>
-                    <Text style={styles.speciality}>{card.speciality}</Text>
-                  </View>
-                </View>
-                <View style={styles.doctor_content}>
-                  <View style={styles.place}>
-                    <FontAwesome
-                      name={'hospital-o'}
-                      size={30}
-                      color="#1c1bad"
-                      style={{ margin: 10 }}></FontAwesome>
-                    <Text style={styles.cardText}>{card.hospitalName}</Text>
-                  </View>
-                  <View style={styles.place}>
-                    <Ionicons
-                      name={'location-sharp'}
-                      size={30}
-                      color="#1c1bad"
-                      style={{ margin: 10 }}></Ionicons>
-                    <Text style={styles.cardText}>{card.hospitalAddress}</Text>
-                  </View>
-                  <View style={styles.customRatingBar}>
-                    <Rating
-                      type="custom"
-                      ratingBackgroundColor="#bfbfbf"
-                      tintColor="#fff"
-                      ratingCount={5}
-                      imageSize={25}
-                      startingValue={card.averageRating}
-                      fractions={1}
-                      readonly={true}
-                      style={{
-                        margin: 5,
-                        backgroundColor: 'transparent',
-                        fontSize: 15,
-                      }}></Rating>
-                  </View>
-                </View>
-              </TouchableOpacity>
-            );
-          })}
-        </ScrollView>
-        <View style={styles.headline}>
-          <Text style={styles.titleText}>Specializations</Text>
-          <Pressable>
-            <Text
-              style={{ color: '#1c1bad', textDecorationLine: 'underline' }}
-              onPress={seeAllSpecializations}>
-              All specializations
-            </Text>
-          </Pressable>
-        </View>
-        <ScrollView vertical={true} showsHorizontalScrollIndicator={false}>
-          {specializations.map((card, cardIndex) => {
-            return (
-              <TouchableOpacity
-                style={styles.specializationCard}
-                key={cardIndex}
-                onPress={() => onPressSpecialization(card)}>
-                <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-                  <FontAwesome
-                    name={'stethoscope'}
-                    size={40}
+  return loadData ? (
+    <View style={styles.loadingIcon}>
+      <ActivityIndicator size="large" color="#0451cc" />
+    </View>
+  ) : (
+    <ScrollView
+      vertical={true}
+      refreshControl={
+        <RefreshControl refreshing={refreshing} onRefresh={onRefreshing} />
+      }>
+      <View style={styles.header}>
+        <Image
+          style={styles.Image}
+          source={require('../../images/app_logo-removebg-preview.png')}></Image>
+        <Text style={styles.headerText}>espitalia</Text>
+      </View>
+      <View style={styles.headline}>
+        <Text style={styles.titleText}>Hospitals</Text>
+        <Pressable>
+          <Text
+            style={{color: '#1c1bad', textDecorationLine: 'underline'}}
+            onPress={seeAllHospitals}>
+            All hospitals
+          </Text>
+        </Pressable>
+      </View>
+      <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+        {hospitals.map((card, cardIndex) => {
+          return (
+            <TouchableOpacity
+              style={styles.cards}
+              key={cardIndex}
+              onPress={() =>
+                onPressHospital(card._id, card.name, card.address)
+              }>
+              <View style={styles.card_header}>
+                <Text style={styles.name}> {card.name} </Text>
+              </View>
+              <View style={styles.hospital_content}>
+                <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                  <Ionicons
+                    name={'location-sharp'}
+                    size={25}
                     color="#1c1bad"
-                    style={{ margin: 10 }}></FontAwesome>
+                    style={{margin: 10}}></Ionicons>
+                  <Text style={styles.cardText}>{card.address}</Text>
                 </View>
-                <View style={{ flex: 2, justifyContent: 'center' }}>
-                  <Text style={styles.cardText}>{card}</Text>
-                </View>
-              </TouchableOpacity>
-            );
-          })}
-        </ScrollView>
+              </View>
+            </TouchableOpacity>
+          );
+        })}
       </ScrollView>
+      {/* <View style={styles.lineStyle} /> */}
+      <View style={styles.headline}>
+        <Text style={styles.titleText}>Doctors</Text>
+        <Pressable>
+          <Text
+            style={{color: '#1c1bad', textDecorationLine: 'underline'}}
+            onPress={seeAllDoctors}>
+            All doctors
+          </Text>
+        </Pressable>
+      </View>
+      <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+        {doctors.map((card, cardIndex) => {
+          return (
+            <TouchableOpacity
+              style={styles.cards}
+              key={cardIndex}
+              onPress={() =>
+                onPressDoctor(
+                  card._id,
+                  card.name,
+                  card.speciality,
+                  card.hospitalName,
+                  card.hospitalAddress,
+                  card.averageRating,
+                )
+              }>
+              <View style={styles.card_header}>
+                <Image
+                  style={styles.doctorImg}
+                  source={{
+                    uri: 'https://bootdey.com/img/Content/avatar/avatar6.png',
+                  }}></Image>
+                <View style={styles.textView}>
+                  <Text style={styles.name}>Dr. {card.name} </Text>
+                  <Text style={styles.speciality}>{card.speciality}</Text>
+                </View>
+              </View>
+              <View style={styles.doctor_content}>
+                <View style={styles.place}>
+                  <FontAwesome
+                    name={'hospital-o'}
+                    size={30}
+                    color="#1c1bad"
+                    style={{margin: 10}}></FontAwesome>
+                  <Text style={styles.cardText}>{card.hospitalName}</Text>
+                </View>
+                <View style={styles.place}>
+                  <Ionicons
+                    name={'location-sharp'}
+                    size={30}
+                    color="#1c1bad"
+                    style={{margin: 10}}></Ionicons>
+                  <Text style={styles.cardText}>{card.hospitalAddress}</Text>
+                </View>
+                <View style={styles.customRatingBar}>
+                  <Rating
+                    type="custom"
+                    ratingBackgroundColor="#bfbfbf"
+                    tintColor="#fff"
+                    ratingCount={5}
+                    imageSize={25}
+                    startingValue={card.averageRating}
+                    fractions={1}
+                    readonly={true}
+                    style={{
+                      margin: 5,
+                      backgroundColor: 'transparent',
+                      fontSize: 15,
+                    }}></Rating>
+                </View>
+              </View>
+            </TouchableOpacity>
+          );
+        })}
+      </ScrollView>
+      <View style={styles.headline}>
+        <Text style={styles.titleText}>Specializations</Text>
+        <Pressable>
+          <Text
+            style={{color: '#1c1bad', textDecorationLine: 'underline'}}
+            onPress={seeAllSpecializations}>
+            All specializations
+          </Text>
+        </Pressable>
+      </View>
+      <ScrollView vertical={true} showsHorizontalScrollIndicator={false}>
+        {specializations.map((card, cardIndex) => {
+          return (
+            <TouchableOpacity
+              style={styles.specializationCard}
+              key={cardIndex}
+              onPress={() => onPressSpecialization(card)}>
+              <View style={{justifyContent: 'center', alignItems: 'center'}}>
+                <FontAwesome
+                  name={'stethoscope'}
+                  size={40}
+                  color="#1c1bad"
+                  style={{margin: 10}}></FontAwesome>
+              </View>
+              <View style={{flex: 2, justifyContent: 'center'}}>
+                <Text style={styles.cardText}>{card}</Text>
+              </View>
+            </TouchableOpacity>
+          );
+        })}
+      </ScrollView>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: 'center' },
+  container: {flex: 1, justifyContent: 'center'},
 
   lineStyle: {
     borderWidth: 1,
@@ -449,6 +443,6 @@ const styles = StyleSheet.create({
   loadingIcon: {
     flex: 1,
     flexDirection: 'row',
-    justifyContent: 'center'
-  }
+    justifyContent: 'center',
+  },
 });
