06dfe7029f0765e583b712d0746eefd145d5b68c
OmarShalaby24 <ramo_24@outlook.com>
2022-06-14 00:59:44 +0200

    finalize and delete blood request Done.

--- src/receptionistPages/BloodReq.js
+++ src/receptionistPages/BloodReq.js
@@ -12,8 +12,10 @@ import {
   TouchableOpacity,
   Modal,
   Pressable,
+  ScrollView,
   RefreshControl,
   Animated,
+  SafeAreaView,
 } from 'react-native';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import Ionicons from 'react-native-vector-icons/Ionicons';
@@ -27,26 +29,14 @@ import EncryptedStorage from 'react-native-encrypted-storage';
 import { Server_URL, Token_Secret, Credintials_Secret } from '@env';
 import { useEffect } from 'react';
 
-const data = [
-  { id: 1, day: 1, month: 'Aug' },
-  { id: 2, day: 2, month: 'Sep' },
-  { id: 3, day: 3, month: 'Sep' },
-  { id: 4, day: 4, month: 'Sep' },
-  { id: 5, day: 5, month: 'Sep' },
-  { id: 6, day: 6, month: 'Sep' },
-  { id: 7, day: 7, month: 'Sep' },
-  { id: 8, day: 8, month: 'Sep' },
-  { id: 9, day: 9, month: 'Sep' },
-];
-
-
 export default function EventsView() {
   const [bloodRequests, setBloodRequests] = useState([{}]);
+  const [oldBloodRequests, setOldBloodRequests] = useState([{}]);
   const [showModal, setShowModal] = useState(false);
   const [refreshing, setRefreshing] = useState(false);
   const onRefresh = useCallback(() => {
     setRefreshing(true);
-    getBloodRequests().then(setRefreshing(false));
+    getBloodRequests().then(getOldBloodRequests().then(setRefreshing(false)));
   }, []);
 
   var token;
@@ -74,9 +64,29 @@ export default function EventsView() {
 
   useEffect(() => {
     getBloodRequests();
+    getOldBloodRequests();
   }, []);
 
+  const getOldBloodRequests = async () => {
+    token = JSON.parse(await EncryptedStorage.getItem(Token_Secret)).token;
+    axios({
+      method: 'get',
+      url: `${Server_URL}:3000/receptionist/getOldBloodRequests`,
+      headers: {
+        'x-auth-token': token
+      }
+    })
+      .then(function (response) {
+        // console.log(response.data);
+        setOldBloodRequests(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
   const finalizeRequest = async (id) => {
+    //TODO: remove from bloodRequests and add to oldBloodRequests
     token = JSON.parse(await EncryptedStorage.getItem(Token_Secret)).token;
     console.log(id);
     axios({
@@ -90,7 +100,6 @@ export default function EventsView() {
       },
     })
       .then(function (response) {
-        // console.log(response.data);
         console.log(response.data);
       })
       .catch(function (error) {
@@ -98,229 +107,184 @@ export default function EventsView() {
       });
   }
 
+  const deleteRequest = async (id) => {
+    token = JSON.parse(await EncryptedStorage.getItem(Token_Secret)).token;
+    console.log(id);
+    axios({
+      method: 'delete',
+      url: `${Server_URL}:3000/receptionist/DropBloodRequest`,
+      headers: {
+        'x-auth-token': token
+      },
+      data: {
+        id: id
+      },
+    })
+      .then(function (response) {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+  }
+
 
   return (
-    <View style={styles.container}>
-      <Modal
-        visible={showModal}
-        transparent
-        onRequestClose={() => setShowModal(false)}>
-        <View style={styles.centeredView}>
-          <View style={styles.modal}>
-            <View style={styles.modalTitle}>
-              <Text style={styles.text}>Enter</Text>
-            </View>
-            <View style={styles.modalBody}>
-              {/* beaker and calender */}
-              <TextInput style={styles.inputFeilds}
-                placeholder='Comments'></TextInput>
-            </View>
-
-            <View style={styles.modalButton}>
-              <Button title='Save' onPress={() => setShowModal(false)}></Button>
-            </View>
-          </View>
-        </View>
-      </Modal>
-
-      <View style={{ alignItems: 'flex-end', paddingTop: 20, paddingRight: 20 }}>
-        <Pressable
-          style={({ pressed }) => [
-            {
-              backgroundColor: pressed ? 'grey' : 'blue',
-            }, styles.addButton
-          ]}
-          onPress={() => setShowModal(true)}>
-          <Icon5 style={styles.newDay} name='plus' />
-        </Pressable>
+    <ScrollView style={styles.container}
+      refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
+    >
+      <View style={{ alignItems: 'center', justifyContent: 'center', marginTop: 10, marginRight: 10 }}>
+        <TouchableOpacity style={styles.touchableOpacity}>
+          <Icon style={styles.addButton} name='plus' />
+          <Text style={{ fontSize: 18, color: '#fff', marginLeft: 10 }}>Make Blood Request</Text>
+        </TouchableOpacity>
       </View>
 
+      <View style={styles.container}>
+        <Text style={{ fontSize: 25 }}>Blood Requests</Text>
+        {bloodRequests.length != 0 ? bloodRequests.map((item, index) => {
+          return (
+            <View style={styles.doctorCard} key={index}>
+              <View style={[styles.doctorView, { flexDirection: "column" }]}>
+                <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', marginBottom: 5 }}>
+                  <View>
+                    <Text style={styles.doctorText}>{item.date}</Text>
+                    <Text style={styles.doctorText}>{item.bloodType}</Text>
 
-      {bloodRequests.length != 0 ? bloodRequests.map((item, cardIndex) => {
-        return (
-          <View >
-            <View style={styles.eventBox}>
-              <View style={styles.eventContent}>
-                <Text style={styles.userName}>Request Donation</Text>
-                <Text style={styles.description}>Date: {item.date}</Text>
-                <Text style={styles.description}>Blood type :{item.bloodType}</Text>
-                <View style={{ flexDirection: 'row' }}>
+
+                  </View>
+                  <View style={{ marginRight: 5 }}>
+                    {item.PatientIDs != null ? <Text style={{ fontSize: 25 }}>{item.PatientIDs.length}</Text> : null}
+                  </View>
+
+                </View>
+                <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
                   <Pressable
-                    disabled={!item.isVisible}
-                    style={({ pressed }) => [
-                      {
-                        backgroundColor: pressed ? 'grey' : 'blue',
-                      }, styles.addButton, styles.button, styles.buttonClose
-                    ]}
+                    style={[styles.button, styles.buttonClose]}
                     onPress={() => {
                       finalizeRequest(item._id);
+                      getBloodRequests();
+                      getOldBloodRequests();
                     }
                     }
                   >
-                    <Icon style={styles.newDay} name='check' />
+                    <Icon style={styles.newDay} name='check-square-o' />
                   </Pressable>
                   <Pressable
                     style={[styles.button, styles.buttonClose, { backgroundColor: "#f00" }]}
                     onPress={() => {
-
+                      deleteRequest(item._id);
+                      getBloodRequests();
+                      getOldBloodRequests();
                     }}
                   >
-                    <Icon style={styles.newDay} name='close' />
+                    <Icon style={styles.newDay} name='trash' />
                   </Pressable>
                 </View>
+
               </View>
             </View>
-          </View>
-        );
-      }) : <Text>Not Found</Text>}
-      {/* <FlatList
-        // enableEmptySections={true}
-        style={styles.eventList}
-        data={bloodRequests}
-        refreshControl={
-          <RefreshControl
-            refreshing={refreshing}
-            onRefresh={onRefresh}
-          />
-        }
-        keyExtractor={(item) => {
-          return item._id;
-        }}
-        renderItem={({ item }) => {
+          )
+        }) : null}
+        <Text style={{ fontSize: 25 }}>Old Blood Requests</Text>
+        {oldBloodRequests.map((item, index) => {
           return (
-            <View visible={item.isVisible}>
-              <View style={styles.eventBox}>
-                <View style={styles.eventContent}>
-                  <Text style={styles.userName}>Request Donation</Text>
-                  <Text style={styles.description}>Date: {item.date}</Text>
-                  <Text style={styles.description}>Blood type :{item.bloodType}</Text>
-                  <View style={{ flexDirection: 'row' }}>
-                    <Pressable
-                      disabled={!item.isVisible}
-                      style={({ pressed }) => [
-                        {
-                          backgroundColor: pressed ? 'grey' : 'blue',
-                        }, styles.addButton, styles.button, styles.buttonClose
-                      ]}
-                      onPress={() => {
-                        finalizeRequest(item._id);
-                      }
-                      }
-                    >
-                      <Icon style={styles.newDay} name='check' />
-                    </Pressable>
-                    <Pressable
-                      style={[styles.button, styles.buttonClose, { backgroundColor: "#f00" }]}
-                      onPress={() => {
+            <View style={styles.doctorCard} key={index}>
+              <View style={[styles.doctorView, { flexDirection: "column" }]}>
+                <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', marginBottom: 5 }}>
+                  <View>
+                    <Text style={styles.doctorText}>{item.date}</Text>
+                    <Text style={styles.doctorText}>{item.bloodType}</Text>
 
-                      }}
-                    >
-                      <Icon style={styles.newDay} name='close' />
-                    </Pressable>
+
                   </View>
+                  <View style={{ marginRight: 5 }}>
+                    {item.PatientIDs != null ? <Text style={{ fontSize: 25 }}>{item.PatientIDs.length}</Text> : null}
+                  </View>
+
                 </View>
               </View>
             </View>
           )
-        }} /> */}
+        })}
 
-    </View>
+      </View>
+
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#DCDCDC",
-    marginBottom: 60,
-  },
-  eventList: {
-    marginTop: 20,
-  },
-  eventBox: {
-    padding: 10,
-    paddingTop: 0,
-    marginTop: 5,
-    marginBottom: 5,
-    flexDirection: 'row',
-  },
-  eventDate: {
-    flexDirection: 'column',
-  },
-  eventDay: {
-    fontSize: 50,
-    color: "#0099FF",
-    fontWeight: "600",
-  },
-  eventMonth: {
-    fontSize: 16,
-    color: "#0099FF",
-    fontWeight: "600",
-  },
-  eventContent: {
     flex: 1,
-    flexDirection: 'column',
-    alignItems: 'flex-start',
-    marginLeft: 10,
-    backgroundColor: '#FFFFFF',
-    padding: 10,
-    borderRadius: 10
+    backgroundColor: 'white',
+    paddingTop: 5,
+    paddingLeft: 5,
+    paddingRight: 5,
   },
-  description: {
-    fontSize: 15,
-    color: "#646464",
-  },
-  eventTime: {
-    fontSize: 18,
-    color: "#151515",
-  },
-  userName: {
-    fontSize: 16,
-    color: "#151515",
-  },
-  modal: {
-    width: 300,
-    height: 300,
-    backgroundColor: '#ffff',
-    borderWidth: 1,
-    borderColor: '#000',
-    borderRadius: 20
-  },
-  centeredView: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#00000099'
-  },
-  modalTitle: {
-    height: 50,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#0099FF',
-    borderTopRightRadius: 20,
-    borderTopLeftRadius: 20,
-  },
-  modalBody: {
-    height: 200,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  modalButton: {
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  inputFeilds: {
-    width: 200,
-    borderWidth: 1,
-    borderColor: '#555',
-    borderRadius: 5,
+  titleStyle: {
+    fontSize: 28,
+    fontWeight: 'bold',
     textAlign: 'center',
-    fontSize: 20,
-    marginBottom: 10,
   },
-  buttonText: {
-    fontSize: 50,
+  input: {
+    height: 40,
+    padding: 8,
+    paddingLeft: 15,
+    paddingRight: 15,
+    margin: 5,
+    fontSize: 16,
+    borderRadius: 50,
+    borderWidth: 1,
+    borderColor: '#ddd',
+    color: 'black',
+    elevation: 1,
+  },
+  touchableOpacity: {
+    // position: 'absolute',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 10,
+    alignItems: 'center',
+    backgroundColor: '#1c1bad',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 7,
+    // width: 'auto',
+    height: 50,
+    borderRadius: 100,
+    // right: 40,
+    // top: 40,
+    elevation: 10,
+
+  },
+  addButton: {
+    fontSize: 30,
     color: 'white',
-    //fontWeight:600,
+  },
+  doctorCard: {
+    padding: 15,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    borderColor: 'gray',
+    margin: 5,
+    marginBottom: 10,
+    elevation: 5,
+  },
+  doctorView: {
+    borderRadius: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    // alignItems: 'center',
+  },
+  doctorText: {
+    fontSize: 15,
+    color: 'black'
+  },
+  icon: {
+    color: 'black',
+    fontSize: 30,
   },
   button: {
     width: 100,
@@ -340,17 +304,8 @@ const styles = StyleSheet.create({
     backgroundColor: "#2196F3",
   },
   newDay: {
-    fontSize: 25,
+    fontSize: 20,
     color: 'white',
-  },
-  addButton: {
-    backgroundColor: '#1c1bad',
-    alignItems: 'center',
-    justifyContent: 'center',
-    position: 'relative',
-    width: 50,
-    height: 50,
-    borderRadius: 100,
-    elevation: 10,
-  },
+
+  }
 });
