a8d3fc99dc4ecd0ebb1cfc0141c142e445717c18
Mayar Adel <48599480+Mayaradel@users.noreply.github.com>
2022-06-19 02:48:33 +0200

    profile page - hospital admin

--- src/hospitalAdminPages/Profilepage.js
+++ src/hospitalAdminPages/Profilepage.js
@@ -1,11 +1,56 @@
-import * as React from 'react';
-import {View, Text, StyleSheet, Image, Button} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View, Text, StyleSheet, Image, Button,BackHandler} from 'react-native';
+import axios from 'axios';
+import EncryptedStorage from 'react-native-encrypted-storage';
+import {Server_URL, Token_Secret, Credintials_Secret} from '@env';
 
 const Profilepage = ({navigation, route}) => {
+   const [hospitalData, setHospitalData] = useState({});
+   const getProfile =  async ()=>{
+    try{
+        const token = JSON.parse(
+            await EncryptedStorage.getItem(Token_Secret),
+          ).token;
+
+          await axios
+          .get(`${Server_URL}:3000/hospital/getProfile`, {
+            headers: {
+              'x-auth-token': token,
+            },
+          }).then(response => {
+            setHospitalData(response.data);
+            console.log(response.data);
+          }) 
+          .catch(function (error) {
+            console.log(error.message);
+          });
+
+    }
+    catch(err){
+        Alert.alert('Error', err.code, [
+            {text: 'Exit', onPress: () => BackHandler.exitApp()},
+          ]);
+
+
+    }
+
+   }
+   useEffect(() =>{
+    getProfile();
+   },[]
+   )
+    
     return (
         <View style={styles.page}>
-            <Image source={require('../../images/logo_withoutBG.png')} style={styles.img} />
-            <Text style={styles.text}>eSpitalia</Text>
+            <Text styles={styles.text}>
+              email: {hospitalData.email}
+            </Text>
+            <Text styles={styles.text}>
+              name: {hospitalData.name}
+            </Text>
+            <Text styles={styles.text}>
+              address: {hospitalData.address}
+            </Text>
             <Button title="Edit" onPress={() => navigation.navigate('MyProfileHospitalPage', {name:"hospital name"})}/> 
         </View>
     );
