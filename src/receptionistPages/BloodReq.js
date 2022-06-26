f9c39d52433dc2536b6dc53df266ca4e246f3faf
OmarShalaby24 <ramo_24@outlook.com>
2022-06-26 03:00:27 +0200

    android and ios icon is added and changed

--- src/receptionistPages/BloodReq.js
+++ src/receptionistPages/BloodReq.js
@@ -175,8 +175,8 @@ export default function EventsView() {
   };
 
   const toggleModal = () => {
-    setShowModal(!showModal);
     setSelectedBloodType('');
+    setShowModal(!showModal);
   };
 
   return (
